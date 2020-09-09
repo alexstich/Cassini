9a9ad48851df3c02e1da1c8aabf4646d5208ed42
AlexStich <alex@rucode.org>
2020-09-09 15:32:41 +0300

    Initial Commit

---
+++ Cassini/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cassini
+//
+//  Created by Алексей Гребенкин on 09.09.2020.
+//  Copyright © 2020 Alex Grebenkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
